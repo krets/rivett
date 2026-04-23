24333a3166a2dcb89e98f051049674ad2ee5287c
Jesse Kretschmer <jesse@krets.com>
2026-04-23 12:10:42 +0200

    fix: DPI awareness

--- build.rs
+++ build.rs
@@ -7,6 +7,19 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winres::WindowsResource::new();
         res.set_icon("resources/icon.ico");
+        // Declare PerMonitorV2 DPI awareness so Windows never virtualises the
+        // window; without this, popup/context-menu surfaces are blurry on
+        // high-DPI displays because the OS applies bitmap scaling.
+        res.set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0" xmlns:asmv3="urn:schemas-microsoft-com:asm.v3">
+  <asmv3:application>
+    <asmv3:windowsSettings>
+      <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true/PM</dpiAware>
+      <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2, PerMonitor</dpiAwareness>
+    </asmv3:windowsSettings>
+  </asmv3:application>
+</assembly>
+"#);
         res.compile().unwrap();
     }
 }
